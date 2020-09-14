9987ba1046dccedcae42b6c5fafec74450f964df
osher sargani <sarganio@post.bgu.ac.il>
2020-09-14 12:23:12 +0300

    finished Simple mode

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const color = document.querySelector('.color');
 btn.addEventListener('click',function(){
 //generate a random index from the range of it length
 const randomIndex = Math.floor(Math.random() * colors.length);
-
-console.log(randomNumber);
-})
+//set the color in background to the random color picked earlier
+document.body.style.backgroundColor = colors[randomIndex];
+//change the name of the color in the page accordingly
+color.textContent = colors[randomIndex];
+console.log(randomIndex);
+});
